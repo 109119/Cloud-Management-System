4ba52a5d67d2cdbb6da0ec8a38d58ba4dee98ffa
andhikayusup <andeeka.ym@gmail.com>
2020-10-25 22:14:43 +0700

    contoh diHitung tambah

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,32 +1,21 @@
 package controller;
 
 import model.Kalkulator;
+import java.net.Socket;
+import java.io.PrintWriter;
 
 public class MainController {
 
     MainView mainView;
+    private String serverPort;
+    private String serverIP;
 
     public MainController(MainView mainView) {
         this.mainView = mainView;
     }
 
     public void hitungTambah(String nilai1, String nilai2) {
-
-        if (nilai1.isEmpty() || nilai2.isEmpty()) {
-
-        } else {
-            long nilaia = Long.parseLong(nilai1);
-            long nilaib = Long.parseLong(nilai2);
-
-            long total = nilaia + nilaib;
-
-            Kalkulator kalkulator = new Kalkulator();
-            kalkulator.setTotal(total);
-
-            mainView.bindView(kalkulator);
-            mainView.emptyTextField();
-        }
-
+        sendRequest(nilai1 " + " nilai2);
     }
 
     public void hitungKurang(String nilai1, String nilai2) {
@@ -87,6 +76,16 @@ public class MainController {
 
         }
 
+    }
+
+    public void sendRequest(String request){
+        Socket s = new Socket(this.serverIP, this.serverPort);
+
+        PrintWriter pr = new PrintWriter(s.getOutputStream());
+        pr.println(request);
+        pr.flush();
+
+        s.close();
     }
 
 }
